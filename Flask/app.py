992e0573bb328f680861796efb07a566e53d5180
Taha Hussain <tahahussain0805@gmail.com>
2025-01-08 10:34:51 +0500

    app file unused codes removed

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, request, Markup
 from markupsafe import Markup
-# from flask_mysqldb import MySQL
 from model import predict_image
 import utils
 import cv2
@@ -10,57 +9,9 @@ import os
 app = Flask(__name__)
 # UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'root'
-# app.config['MYSQL_DB'] = 'flask'
  
-# mysql = MySQL(app)
-
 @app.route('/', methods=['GET'])
 def home():
-    path=os.path.join(app.root_path,'static','cam')
-    # path = "/home/waqas/Documents/FYP II/Plant_AI-master/fyp-ii/Flask/static/cam"
-    cam_port = 0
-    a = 0
-    cam = cv2.VideoCapture('rtsp://admin:Talha1234@192.168.100.48/H264?ch=1&subtype=0')
-    # Define the codec and create VideoWriter object
-    # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    # out = cv2.VideoWriter('/home/Documents/FYP II/Plant_AI-master/fyp-ii/Flask/static/cam/output.mp4', fourcc, 20.0, (640, 480))
-    if cam.isOpened():
-        while(True):
-            # reading the input using the camera
-            result, image = cam.read()
-            
-            # If image will detected without any error, 
-            # show result
-            # Converts to grayscale space, OCV reads colors as BGR
-            # frame is converted to gray
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            cv2.rectangle(image,(900,450),(500,200),(0,255,0),6)
-
-            # output the frame
-            # out.write(gray)
-            
-            # showing result, it take frame name and image 
-            # output
-            cv2.imshow("FYP-II", image)
-
-            cv2.imshow("Gray", gray)
-
-            # saving image in local storage
-            # Passing image path in first parameter of imwrite
-            cv2.imwrite(path+"/FYP"+str(a)+".jpg", image)
-            a = a + 1
-            # If keyboard interrupt occurs, destroy image 
-            # window
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    cam.release()
-    cv2.destroyAllWindows()
-
     try:
         # img = "C:/Users/DELL/Desktop/Disease Detection/test/test/PotatoEarlyBlight3.jpeg"
         img = os.path.join(app.root_path,'TestImages','PotatoEarlyBlight3.jpeg')
@@ -71,12 +22,7 @@ def home():
         return render_template('display.html', status=200, result=res)
     except:
         pass
-    
-    test='abc'
-    # cur = mysql.connection.cursor()
-    # cur.execute("INSERT INTO test(img) VALUES (%s)", [test])
-    # mysql.connection.commit()
-    # cur.close()
+
     
     # If captured image is corrupted, moving to else part
     return render_template('index.html')
@@ -137,8 +83,8 @@ def predict():
             print(prediction, 'prediction')
             res = Markup(utils.disease_dic[prediction])
             print(res, 'res')
-
-            return render_template('display.html', status=200, result=res)
+            return jsonify(res)
+            # return render_template('display.html', status=200, result=res)
         except Exception as e:
             print("Error occurred:", e)
             # traceback.print_exc()
